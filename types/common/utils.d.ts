bfa1a4d9480ab6dfe6f1b3ca126044bccb82c21d
Ravenyjh <jiahaoyan20172@gmail.com>
2024-10-19 21:23:29 +0800

    Fix: Update package entry, add types config, and remove Node.js dependencies
    - Update the package entry configuration to point correct path.
    - Add 'types' configuration for usage in TypeScript code.
    - Remove Node.js dependent methods like 'fs' to ensure compatibility with web environments.

--- types/common/utils.d.ts
+++ types/common/utils.d.ts
@@ -3,4 +3,3 @@ export function bytesToBigint(bytes: any): bigint;
 export function convertToBiguint64(timestamp: any): bigint;
 export function formatArray(arr: any): string;
 export function jsonifyData(data: any, useBigInt?: boolean): string;
-export function writeJsonToFile(data: any, filename: any): Promise<string>;
